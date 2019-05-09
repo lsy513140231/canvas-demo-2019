d19c4af59669d4fa6c9984af990898f6714bbf2b
victor <444849999@qq.com>
2019-05-10 01:21:31 +0800

    新增添加颜色按钮

--- main.js
+++ main.js
@@ -1,35 +1,42 @@
+//用户鼠标的状态
+var userState = 'drow';
+//用户笔的颜色
+var pencolor = 'black';
+
+//清理点击事件
 document.body.ontouchstart = function(click) {
     click.preventDefault();
 }
-var userState = 'drow';
-var pencolor = 'black'
-    //设置画板大小
+
+document.getElementById("addColor").onclick = function(click) {
+    document.getElementById("colorPanl_Bg").classList.add(".actions");
+}
+
+//颜色集合
+var myColors = [
+    { r: 0, g: 0, b: 0 },
+    { r: 255, g: 0, b: 0 },
+    { r: 0, g: 255, b: 0 },
+    { r: 0, g: 0, b: 255 },
+]
+
+//添加颜色，并且添加点击事件
+InitColor();
+
+
+//RGB转16进制
+function colorRGB2Hex(color) {
+    var r = parseInt(color.r);
+    var g = parseInt(color.g);
+    var b = parseInt(color.b);
+    var hex = "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    return hex;
+}
+
+//设置画板大小
 var canvas = document.getElementById("canvas2");
 autoSetCanvasSize(canvas);
 var context = canvas.getContext('2d');
-
-//设置画笔颜色按钮
-red.onclick = function() {
-    pencolor = 'red';
-    red.classList.add("select");
-    green.classList.remove("select");
-    blue.classList.remove("select");
-    btn_pen.onclick();
-}
-green.onclick = function() {
-    pencolor = 'green';
-    red.classList.remove("select");
-    green.classList.add("select");
-    blue.classList.remove("select");
-    btn_pen.onclick();
-}
-blue.onclick = function() {
-    pencolor = 'blue';
-    red.classList.remove("select");
-    green.classList.remove("select");
-    blue.classList.add("select");
-    btn_pen.onclick();
-}
 
 //清除按钮
 clear.onclick = function() {
@@ -55,6 +62,8 @@ btn_Eraser.onclick = function(btn) {
     mousedown = false;
     touchdown = false;
 }
+
+
 
 //笔的点击事件
 var btn_pen = document.getElementById("pen");
@@ -139,6 +148,8 @@ function msdown(a) {
         case 'eraser':
             context.clearRect(x - 5, y - 5, 10, 10);
             break;
+        case "none":
+            break;
     }
 }
 
@@ -156,6 +167,8 @@ function msmove(b) {
             break;
         case 'eraser':
             context.clearRect(x - 5, y - 5, 10, 10);
+            break;
+        case "none":
             break;
     }
     lastPos.x = x;
@@ -179,7 +192,7 @@ function todown(a) {
     var x = a.touches[0].clientX;
     var y = a.touches[0].clientY;
     touchdown = true;
-    console.log(x + "   " + y);
+    //console.log(x + "   " + y);
     switch (userState) {
         case 'drow':
             lastPos2.x = x;
@@ -193,6 +206,7 @@ function todown(a) {
 }
 //触屏中
 function tomove(b) {
+
     if (!touchdown) {
         return;
     }
@@ -209,6 +223,83 @@ function tomove(b) {
     }
     lastPos2.x = x;
     lastPos2.y = y;
+    //b.preventDefault();
 }
 //触屏结束
 function toup(c) { touchdown = false; }
+
+
+
+function InitColor() {
+    if (localStorage.getItem('colors')) {
+        var hashInlocalStorage = JSON.parse(localStorage.getItem('colors'));
+        if (hashInlocalStorage) {
+            myColors = hashInlocalStorage;
+        }
+    }
+
+
+    var Element_colors = document.getElementById("colors");
+
+    for (var index = 0; index < myColors.length; index++) {
+        var child = document.getElementById("myColor" + index);
+        if (child) {
+            Element_colors.removeChild(child);
+        }
+        var li = document.createElement("li");
+        Element_colors.appendChild(li);
+        li.style = "background: rgb(" + myColors[index]["r"] + "," + myColors[index]["g"] + "," + myColors[index]["b"] + ");transition: all 0.3s;";
+        li.id = "myColor" + index;
+        if (index === 0) {
+            li.classList.add("select");
+        }
+    }
+
+    for (var index = 1; index < Element_colors.children.length; index++) {
+        Element_colors.children[index].onclick = function(eee) {
+
+            for (var index1 = 1; index1 < Element_colors.children.length; index1++) {
+                if (eee.target === Element_colors.children[index1]) {
+                    Element_colors.children[index1].classList.add("select");
+                    pencolor = colorRGB2Hex(myColors[index1 - 1]);
+                } else {
+                    Element_colors.children[index1].classList.remove("select");
+                }
+            }
+        }
+    }
+}
+
+var colorPanl_Color_r = document.getElementById("colorPanl_Color_r");
+var colorPanl_Color_g = document.getElementById("colorPanl_Color_g");
+var colorPanl_Color_b = document.getElementById("colorPanl_Color_b");
+colorPanl_Color_r.onchange = changeColor;
+colorPanl_Color_g.onchange = changeColor;
+colorPanl_Color_b.onchange = changeColor;
+
+function changeColor() {
+
+    var colorPanl = document.getElementById("colorPanl_Color");
+    colorPanl.style = "background:rgb(" + colorPanl_Color_r.value + "," + colorPanl_Color_g.value + "," + colorPanl_Color_b.value + ")";
+}
+
+//
+var btn_addcolor = document.getElementById("addColor");
+btn_addcolor.onclick = function(eee) {
+    document.getElementById("colorPanl_Bg").classList.add("actions");
+}
+
+//
+var btn_cancel = document.getElementById("colorPanl_cancel");
+btn_cancel.onclick = function(eee) {
+    document.getElementById("colorPanl_Bg").classList.remove("actions");
+}
+
+//
+var btn_ok = document.getElementById("colorPanl_ok");
+btn_ok.onclick = function(eee) {
+    myColors[myColors.length] = { r: colorPanl_Color_r.value, g: colorPanl_Color_g.value, b: colorPanl_Color_b.value };
+    InitColor();
+    document.getElementById("colorPanl_Bg").classList.remove("actions");
+    localStorage.setItem('colors', JSON.stringify(myColors));
+}
